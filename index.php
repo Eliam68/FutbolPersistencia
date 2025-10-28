5b488dfec9fe0db87251455c3e26673f97f50c4a
Eliam68 <ian681968@gmail.com>
2025-10-28 12:32:38 +0100

    Arquitectura como la primera versión de Artean.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @title: Entrega Futbol con Persistencia
+ * @description:  Página PHP donde crear y poblar una base de datos de fútbol con persistencia, gestionando equipos, partidos y resultados mediante claves foráneas e índices optimizados.
+ * @version    0.1
+ *
+ * @author Eliam Carril
+ */
+
+
+require_once __DIR__ . '/templates/footer.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Entrega Futbol con Persistencia</title>
+</head>
+<body>
+  <div class="container">
+    <h1 class="mt-4">Entrega Futbol con Persistencia</h1>
+    <button class="btn btn-primary">Equipos</button>
+    <button class="btn btn-primary">Partidos</button>
+  </div>
+</body>
+</html>
---
+++ persistence/DAO/GenericDAO.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../conf/PersistentManager.php';
+
+abstract class GenericDAO {
+
+  //Conexión a BD
+  protected $conn = null;
+  //Constructor de la clase
+  public function __construct() {
+
+    $this->conn = PersistentManager::getInstance()->get_connection();
+  }
+
+
+}
